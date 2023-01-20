aa1a5249630a2af71705bbf910611ad9794e6934
Philipp Tuchardt <philipp.tuchardt@nahketing.de>
2023-01-20 13:22:05 +0100

    feat(core): moved external dependencies to local directory

---
+++ js/submit-form.js
@@ -0,0 +1,4 @@
+document.getElementById("submit-form").addEventListener("submit", function (event) {
+    event.preventDefault();
+    alert("Thank you for reaching out to our landscaping company! A representative will be in touch with you shortly.");
+});
